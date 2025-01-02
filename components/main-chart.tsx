0049a23f9b847bedbb469abad2d28135307dbf2d
Всеволод <kreepmeister@gmail.com>
2025-01-02 16:57:16 +0000

    fix: text and border in chart

--- components/main-chart.tsx
+++ components/main-chart.tsx
@@ -16,7 +16,7 @@ const chartConfig = {
     label: "кол-во",
   },
   new_users: {
-    label: "Пользователи",
+    label: "Пользователей",
     color: "hsl(var(--chart-1))",
   },
   new_hosts: {
@@ -97,13 +97,13 @@ export function MainChart() {
               <button
                 key={chart}
                 data-active={activeChart === chart}
-                className="relative z-30 flex flex-1 flex-col justify-center gap-1 px-6 py-4 text-left even:border-l data-[active=true]:bg-muted/50 sm:border-l sm:border-t-0 sm:px-8 sm:py-6"
+                className="relative z-30 flex flex-1 flex-col justify-center gap-1 border-l px-6 py-4 text-left first:border-l-0 data-[active=true]:bg-muted/50 sm:border-t-0 sm:px-8 sm:py-6"
                 onClick={() => setActiveChart(chart)}
               >
                 <span className="text-xs text-muted-foreground">
                   {chartConfig[chart].label}
                 </span>
-                <span className="text-lg font-bold leading-none sm:text-3xl">
+                <span className="text-sm font-bold leading-none sm:text-3xl">
                   {total[chart].toLocaleString()}
                 </span>
               </button>
